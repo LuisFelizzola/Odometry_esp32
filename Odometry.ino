b77fd3fd26ef58fab284480a3304e711c61cbf95
LuisFelizzola <luanfecas@gmail.com>
2023-09-13 16:46:43 -0500

    Primer commit

--- Odometry.ino
+++ Odometry.ino
@@ -1,103 +1,124 @@
+#include <Robot.h> //importo la clase robot y rueda
+#include <Wheel.h>
+#include <Odometry.h> //importo las funciones de la odometria
 /*
-
-
+PINS I CANNOT USE
+5,16,17,35
+PINES DEL MOTOR1: 2,4,14 . ENCODER MOTOR 1:A-27, B-32.
+PINES DEL MOTOR 2: 18,19,21 ENCODER MOTOR 2: A-33, B-34.
 CODIGO PARA LEER LA VELOCIDAD Y ESTIMAR LA POSICION DEL ROBOT
 */
 
+//RUEDAS DELANTERAS
 #define motor1A 2
 #define motor1B 4
 #define en1 14
-#define motor2A 16
-#define motor2B 17
-#define en2 18
-#define motor3A 19
-#define motor3B 21
-#define en3 22
-#define motor4A 23
-#define motor4B 25
-#define en4 26
-#define encoderA1 27
+#define motor2A 18
+#define motor2B 19
+#define en2 21
+//RUEDAS TRASERAS
+#define motor3A 22
+#define motor3B 23
+#define en3 25
+#define motor4A 13
+#define motor4B 12
+#define en4 15
+
+#define encoderA1 33
 #define encoderB1 32
-#define encoderA2 33 
+#define encoderA2 27 
 #define encoderB2 34
+
 #define encoderA3 35
-#define encoderB3 36 
-#define encoderA4 37 
-#define encoderB4 38
+#define encoderB3 5
+#define encoderA4 36
+#define encoderB4 39
 
 const int freq=3000;
 const int pwmChannel=0,pwmChannel2=1,pwmChannel3=2,pwmChannel4=3;
 const int resolution=8;
-long prevT1=0,prevT2=0,prevT3=0,prevT4=0; //tiempo previo
+long prevT=0; //tiempo previo
 volatile int pos_i1=0,pos_i2=0,pos_i3=0,pos_i4=0; //posicion de la rueda 1 a la 4
 int prevPos1=0, prevPos2,prevPos3=0,prevPos4=0;
-
-TaskHandle_t Tarea0,Tarea1,Tarea2,Tarea3;
-
+float r=0.04; //radio de la llanta
+float Lx=0.116, Ly=0.055; //MITAD DE LA DISTANCIA ENTRE LAS LLANTAS TRASERAS Y LAS LLANTAS DELANTERAS (m)
 //variables para el filtro pasa-bajas con frecuencia de corte de 25Hz
-
 float vFilt1=0,vFilt2=0,vFilt3=0,vFilt4=0;
 float vPrev1=0,vPrev2=0,vPrev3=0,vPrev4=0;
-
-
+int dtc=250;
+struct odometry odom;
+//posicion Inicial del robot:
+float xi=0, yi=0,tetai=0;
+char mv=' '; //variable para validar que tipo de movimiento esta Mercury (pruebas)
+TaskHandle_t Tarea0,Tarea1,Tarea2,Tarea3;
+Wheel wh1(motor1A, motor1B, en1,r, pwmChannel);
+Wheel wh2(motor2A, motor2B, en2,r, pwmChannel2);
+Wheel wh3(motor3A, motor3B, en3,r, pwmChannel3);
+Wheel wh4(motor4A, motor4B, en4,r, pwmChannel4);
+//DEFINO LOS PARAMETROS DEL OBJETO DE MI CLASE ROBOT (CREADA PARA EL ROBOT MECANUM )
+Robot Mercury(&wh1,&wh2,&wh3,&wh4,xi,yi,tetai);
 void setup() {
 
-  xTaskCreatePinnedToCore(readEncoder1,"Tarea_0",1000,NULL,1,&Tarea0,1);
-  xTaskCreatePinnedToCore(readEncoder2,"Tarea_1",1000,NULL,1,&Tarea1,1);
-  xTaskCreatePinnedToCore(readEncoder3,"Tarea_2",1000,NULL,1,&Tarea2,1);
-  xTaskCreatePinnedToCore(readEncoder4,"Tarea_3",1000,NULL,1,&Tarea3,1);
-
-
+  xTaskCreatePinnedToCore(printing,"Tarea_0",1000,NULL,1,&Tarea0,0);
+  attachInterrupt(encoderA1, readEncoder1, RISING);
+  attachInterrupt(encoderA2, readEncoder2, RISING);
+  attachInterrupt(encoderA3, readEncoder3, RISING);
+  //attachInterrupt(encoderA4, readEncoder4, RISING);
   setPins(); // configuro cuáles son mis entradas y salidas
 
   Serial.begin(115200);
 
 }
 //LEO EL ENCODER DEL MOTOR 1
-void readEncoder1(void *parameter){
-
-  while(1==1){
+void readEncoder1(){//void *parameter
     int b= digitalRead(encoderB1);
     pos_i1=incrementarPos(b,pos_i1);
-    delayMicroseconds(111); //cada 111 us
-  }
 }
 //LEO EL ENCODER DEL MOTOR 2
-void readEncoder2(void *parameter){
-  while(1==1){
+void readEncoder2(){//void *parameter
     int b= digitalRead(encoderB2);
     pos_i2=incrementarPos(b,pos_i2);
-    delayMicroseconds(110); //cada 110 us
+}
+void printing(void *parameter){
+  while(1==1){
+    char a=Serial.read();
+    if(a=='F'){ //forward (same velocyty for all of them)
+    Mercury.moveForward(dtc); //metodo de la clase Robot, solo necesita como parametro la velocidad
+    mv='F';
+    }else if (a=='B'){ //backward (same velocyty for all of them)
+    Mercury.moveBackward(dtc);
+    mv='B';
+    }else if (a=='R'){ //right (u1=u3, u2=u4). u1=-u2
+    Mercury.moveRight(dtc);
+    mv='R';
+    }else if (a=='L'){ //left
+    Mercury.moveLeft(dtc);
+    mv ='L';
+    }else if(a=='I'){
+    Mercury.rotation(dtc);
+    mv ='I';
+    }else if(a=='S'){ //stop
+    Mercury.stop();
+    mv ='S';
+    }else if(a=='D'){
+      dtc=50;
+    }
+    delay(200);
   }
 }
 //LEO EL ENCODER DEL MOTOR 3
-void readEncoder3(void *parameter){
-  while(1==1){
-     int b= digitalRead(encoderB3);
-    pos_i3=incrementarPos(b,pos_i3);
-    delayMicroseconds(112); //cada 112 us   
-  }
+
+void readEncoder3(){
+  int b= digitalRead(encoderB3);
+  pos_i3=incrementarPos(b,pos_i3);
 }
 
 //LEO EL ENCODER DEL MOTOR 4
-void readEncoder4(void *parameter){
-  while(1==1){
-     int b= digitalRead(encoderB4);
-    pos_i4=incrementarPos(b,pos_i4);
-    delayMicroseconds(113); //cada 113 us   
-  }
+void readEncoder4(){
+  int b= digitalRead(encoderB4);
+  pos_i4=incrementarPos(b,pos_i4);
 }
 
-//funcion para incrementar la posicion de cada rueda cuando el flanco del encoder es activado
-int incrementarPos(int b,int posI){
-  int i=0;
-  if (b>0){ //si el motor se mueve counter clockwise
-    i=1;
-  }else{
-    i=-1;
-  }
-  return posI+i; //retorno el valor de la posición inicial más el incremento
-}
 
 //función para configurar mis entradas y salidas
 void setPins(){
@@ -143,15 +164,11 @@ void setPins(){
   //Relacionar los pines a ese canal
   ledcAttachPin(en1,pwmChannel);
   ledcAttachPin(en2,pwmChannel2);
-  ledcAttachPin(en3,pwmChannel3);
-  ledcAttachPin(en4,pwmChannel4);
+  ledcAttachPin(en3, pwmChannel3);
+  ledcAttachPin(en4, pwmChannel3);
+
 }
 void loop() {
-  //move forward
-  setMotor(motor1A,motor1B,en1,200,1,0,pwmChannel);
-  setMotor(motor2A,motor2B,en2,200,1,0,pwmChannel2);
-  setMotor(motor3A,motor3B,en3,200,1,0,pwmChannel3);
-  setMotor(motor4A,motor4B,en4,200,1,0,pwmChannel4);
   long currentTime=micros();
   //guardar posicion de los motores, en un bloque átomico para guardar las posiciones,desactivo las interrupciones (por si alguna ocurre).
   int pos1=0,pos2=0,pos3=0,pos4=0;
@@ -162,44 +179,74 @@ void loop() {
   pos4=pos_i4;
   interrupts(); //las vuelvo a activar, una vez llene los datos de la posición
   //calculate the veloocity in RPM and save it
-  //I know the encoder resolution is: 1 rev/2ct. ct/s-*rev/2ct *60s/min
-  float velocity1=calculateVel(currentTime,prevPos1,pos1,prevT1,vFilt1,vPrev1)*(60/2);
-  prevT1=currentTime;
+  //I know the encoder resolution is: 1 rev/2ct. ct/s*rev/2ct *60s/min
+  //RESOLUTION OF THE ENCODER 1 rev/ 12 ct.
+  /*  CODIGO PARA PROBAR LA CALIDAD DE LOS 4 ENCODERS, SE ESCOGE EL MEJOR DE LOS 4
+  float w1=calculateW(currentTime,prevPos1,pos1,prevT,vFilt1,vPrev1)*12; //rev/s
+  float w2=calculateW(currentTime,prevPos2,pos2,prevT,vFilt2,vPrev2)*12; //ct /s o rev/s
+  float w3=calculateW(currentTime,prevPos3,pos3,prevT,vFilt3,vPrev3)*12; //rev/s
+  float w4=calculateW(currentTime,prevPos4,pos4,prevT,vFilt4,vPrev4)*12; //ct/s  
+  */
+  float w1 =calculateW(currentTime,prevPos1,pos1,prevT,vFilt1,vPrev1)*12; //el encoder con mejor rendimiento
+  float w2,w3,w4;
   prevPos1=pos1;
-  float velocity2=calculateVel(currentTime,prevPos2,pos2,prevT2,vFilt2,vPrev2)*(60/2);
-  prevT2=currentTime;
+  /*
   prevPos2=pos2;
-  float velocity3=calculateVel(currentTime,prevPos3,pos3,prevT3,vFilt3,vPrev3)*(60/2);
-  prevT3=currentTime;
   prevPos3=pos3;
-  float velocity4=calculateVel(currentTime,prevPos4,pos4,prevT4,vFilt4,vPrev4)*(60/2);
-  prevT4=currentTime;
   prevPos4=pos4;
+  */
+  switch (mv){ //al ser el movimiento de Mercury reestringido, asi debe ser mis calculos para cumplir con la reestriccion en las formulas
+    
+    case 'F': //forward or bakward
+    case 'B':
+    
+         w2 =w1;
+         w3= w1;
+         w4= w1;
+        break;
+    case 'R': //sideways
+    case 'L':
+         w2=-w1;
+         w3=w1;
+         w4=w2;
+        break;
+    case 'I': //rotation
+         w2=-w1;
+         w3=w2;
+         w4=w1;
+        break;
+      
+    case 'S': //stop
+        w1=0;
+        w2=0;
+        w3=0;
+        w4=0;
+        pos1=0;
+        break;
+  }
+  Mercury.wheel_1->setVelocity(w1);
+  Mercury.wheel_2->setVelocity(w2);
+  Mercury.wheel_3->setVelocity(w3);
+  Mercury.wheel_4->setVelocity(w4);
+//VELOCIDAD DE LA RUEDA EN RADIANES:
+  float w1r=Mercury.wheel_1->Velocityradians(); //para que pueda guardar todo el calculo posterior en una variable float (numero muy grande)
+  float w2r=Mercury.wheel_2->Velocityradians(); 
+  float w3r=Mercury.wheel_3->Velocityradians();
+  float w4r=Mercury.wheel_4->Velocityradians();   
+  forwardKinematics(odom, w1r, w2r, w3r, w4r,  r,  Lx,  Ly,  prevT, currentTime); //estimo la posicion
+  prevT=currentTime;
 
-  Serial.print(velocity1);
-  Serial.println();
-  Serial.print(velocity2);
-  Serial.println();
-  Serial.print(velocity3);
-  Serial.println();
-  Serial.print(velocity4);
+  Mercury.setPosition(odom.x, odom.y, odom.teta); //configuro la posicion de Mercury
+  Serial.print(odom.x);
+  Serial.print(" ");
+  Serial.print(odom.y);
+  Serial.print(" ");
+  Serial.print(odom.teta);
+  //Serial.print(" ");
+  //Serial.print(w4r);
   Serial.println();
   delay(200);
 }
-//funcion para encender un motor
-void setMotor(int inA,int inB, int enA, int dutycicle, int state1,int state2,int pwmCh){
-    digitalWrite(inA,state1);
-    digitalWrite(inB,state2);
-    ledcWrite(pwmCh,dutycicle);
-}
-//calculo la velocidad teniendo en cuenta que v= dx/dt. Además aplico el filtro pasa bajas con frecuencia de corte de 25hz
-float calculateVel(long ct,int prevPos, int pos, long prevT,float &vFlt, float &vPrev){
-  float vel=0;
-  float deltaT=((float)(ct-prevT))/1.0e6; //paso el dx a s
-  vel= (pos-prevPos)/deltaT;
 
-  //aplico el filtro pasa-bajas
-  vFlt=0.854*vFlt+0.0728*vel+0.0728*vPrev;
-  vPrev=vel;  
-  return vFlt;
-}
+
+
